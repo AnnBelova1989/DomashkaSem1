90fc6148278bf0e606f1a75337987003b500cdfa
AnnBelova <anutik-89@tut.by>
2023-08-07 14:38:45 +0300

    задача 8

---
+++ zd4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int A = 2;
+while(A<=N)
+{
+    System.Console.Write(A);
+if(A < N-1)
+{
+    Console.Write(", ");
+}
+ A=A+2;
+}
