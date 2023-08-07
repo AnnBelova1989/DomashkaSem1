e3cf82146cf10dc78482b4460400b77de374b88d
AnnBelova <anutik-89@tut.by>
2023-08-07 15:07:16 +0300

    задача 4

---  zd2/Program.cs
+++  zd2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+ if(num1>max)
+ {
+    max=num1;
+ }
+if(num2>max)
+{
+    max=num2;
+}
+if(num3> max)
+{
+    max=num3;
+}
+Console.Write($"Максимальным числом среди чисел {num1}, {num2}, {num3} является { max} ");
+
+
