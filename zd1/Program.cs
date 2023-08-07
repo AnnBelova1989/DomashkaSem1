3cfcc9fbcab9fbb08a643344531a4ab40b9d4d8d
AnnBelova <anutik-89@tut.by>
2023-08-07 15:12:35 +0300

    задача 2

--- zd1/Program.cs
+++ zd1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+ if(num1>num2)
+ {
+    Console.Write($"Наибольшим числом среди чисел {num1}, {num2} является { num1}, наименьшим {num2} ");
+ }
+ else
+{
+    Console.Write($"Наибольшим числом среди чисел {num1}, {num2} является { num2}, наименьшим {num1} ");
+}
